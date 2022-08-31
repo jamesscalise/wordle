04a9b92d5dd7c434cd9342af3c4ed0d5b8b5c3af
James Scalise <james.scalise@oracle.com>
2022-08-31 15:26:16 -0500

    add handlekey state change

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import './App.css';
-import {useEffect, useState} from "react";
+import { useEffect, useState } from 'react';
+import Wordle from './components/Wordle';
 
 function App() {
+	const [ solution, setSolution ] = useState(null);
 
-  const [solution, setSolution] = useState(null);
+	useEffect(
+		() => {
+			fetch('http://localhost:3001/solutions').then((res) => res.json()).then((json) => {
+				const randomSolution = json[Math.floor(Math.random() * json.length)];
+				setSolution(randomSolution.word);
+			});
+		},
+		[ setSolution ]
+	);
 
-  useEffect(() => {
-    fetch('http://localhost:3001/solutions').then(res => res.json()).
-    then(json => {
-      const randomSolution = json[Math.floor(Math.random()*json.length)]
-      setSolution(randomSolution.word)
-    });
-  }, [setSolution])
-
-  return (
-    <div className="App">
-        <h1>Wordle!  </h1>
-        {solution && <div>Solution is: {solution}</div>}
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Wordle! </h1>
+			{solution && <Wordle solution={solution} />}
+		</div>
+	);
 }
 
 export default App;
--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -1,27 +1,34 @@
-import { useState } from "react"
+import { useState } from 'react';
 
-const useWorlde = (solution) => {
-    const [turn, setTurn] = useState(0)
-    const [currentGuess, setCurrentGuess] = useState('')
-    const [guesses, setGuesses] = useState([])
-    const [history, setHistory] = useState([])
-    const [isCorrect, setIsCorrect] = useState(false)
+const useWordle = (solution) => {
+	const [ turn, setTurn ] = useState(0);
+	const [ currentGuess, setCurrentGuess ] = useState('');
+	const [ guesses, setGuesses ] = useState([]);
+	const [ history, setHistory ] = useState([]);
+	const [ isCorrect, setIsCorrect ] = useState(false);
 
+	const formatGuess = () => {};
 
+	const addNewGuess = () => {};
 
-    const formatGuess = () => {
+	const handleKeyUp = ({ key }) => {
+		console.log(key);
+		if (key === 'Backspace') {
+			setCurrentGuess((prev) => {
+				return prev.slice(0, -1);
+			});
+			return;
+		}
+		if (/^[A-Za-z]$/.test(key)) {
+			if (currentGuess.length < 5) {
+				setCurrentGuess((prev) => {
+					return prev + key;
+				});
+			}
+		}
+	};
 
-    }
+	return { turn, currentGuess, guesses, history, isCorrect, handleKeyUp };
+};
 
-    const addNewGuess = () => {
-
-    }
-
-    const handleKeyUp = () => {
-
-    }
-
-    return {turn, currentGuess, guesses, history, isCorrect}
-}
-
-export default useWorlde
+export default useWordle;

164a7a6e2799d8ff63c035add36330a541e70f24
James Scalise <james.scalise@oracle.com>
2022-09-01 17:05:23 -0500

    formatted guess

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -7,12 +7,48 @@ const useWordle = (solution) => {
 	const [ history, setHistory ] = useState([]);
 	const [ isCorrect, setIsCorrect ] = useState(false);
 
-	const formatGuess = () => {};
+	const formatGuess = () => {
+		console.log('formatting');
+		let solutionArray = [ ...solution ];
+		let formattedGuess = [ ...currentGuess ].map((letter) => {
+			return { key: letter, color: 'grey' };
+		});
+
+		formattedGuess.forEach((letter, i) => {
+			if (solutionArray[i] === letter.key) {
+				formattedGuess[i].color = 'green';
+				solutionArray[i] = null;
+			}
+		});
+
+		formattedGuess.forEach((letter, i) => {
+			if (solutionArray.includes(letter.key) && letter.color !== 'green') {
+				formattedGuess[i].color = 'yellow';
+			}
+		});
+		console.log(formattedGuess);
+		setCurrentGuess('');
+		return formattedGuess;
+	};
 
 	const addNewGuess = () => {};
 
 	const handleKeyUp = ({ key }) => {
 		console.log(key);
+		if (key === 'Enter') {
+			if (turn > 5) {
+				return;
+			}
+			if (history.includes(currentGuess)) {
+				return;
+			}
+
+			if (currentGuess.length !== 5) {
+				return;
+			}
+
+			console.log(formatGuess());
+		}
 		if (key === 'Backspace') {
 			setCurrentGuess((prev) => {
 				return prev.slice(0, -1);
